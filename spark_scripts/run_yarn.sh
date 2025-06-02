11be2a4c8f899923e75ea4dc2de9a4de1b7dd4e0
Isabella Callejas <isabella.callejas.m@gmail.com>
2025-06-02 17:35:49 +0000

    Yolo ajustada a que funcione en hadoop de la u

--- spark_scripts/run_yarn.sh
+++ spark_scripts/run_yarn.sh
@@ -2,12 +2,9 @@
 spark-submit \
   --master yarn \
   --deploy-mode cluster \
-  --archives spark_env.tar.gz#env \
+  --archives env.tar.gz#env \
   --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=./env/bin/python \
   --conf spark.executorEnv.PYSPARK_PYTHON=./env/bin/python \
   --py-files spark_scripts/spark_job.zip \
-  --files \
-    spark_scripts/data/input_images.txt,\
-    spark_scripts/data/tile_metadata.json,\
-    spark_scripts/models/yolo_best_fine_tune_800.pt \
+  --files spark_scripts/data/input_images.txt,spark_scripts/data/tile_metadata.json,spark_scripts/models/yolo_best_fine_tune_800.pt \
   spark_scripts/spark_job/distributed_inference.py
